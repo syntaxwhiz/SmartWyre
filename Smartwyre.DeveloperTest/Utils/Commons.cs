9b57dfca340a459a08639f2d86ba44ccfd66bf21
syntaxwhiz <zamaan.net.dev@gmail.com>
2022-06-13 20:43:10 +0500

    Commons: Added Methods

---
+++ Smartwyre.DeveloperTest/Utils/Commons.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smartwyre.DeveloperTest.Utils
+{
+    public class Commons
+    {
+        public static void Error(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
