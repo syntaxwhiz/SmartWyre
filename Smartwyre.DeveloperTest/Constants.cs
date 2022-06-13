b3ce380d5bd93b2ed09ec9454434d84b166c5d38
syntaxwhiz <zamaan.net.dev@gmail.com>
2022-06-13 20:43:10 +0500

    Constants : Added Connection String of our SQL Server database.

---
+++ Smartwyre.DeveloperTest/Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace Smartwyre.DeveloperTest
+{
+    public class Constants
+    {
+        readonly static public string CONNECTION_STRING = "Data Source=DESKTOP-FS7BTL7;Initial Catalog=Smartwyre;Integrated Security=True";
+    }
+}
