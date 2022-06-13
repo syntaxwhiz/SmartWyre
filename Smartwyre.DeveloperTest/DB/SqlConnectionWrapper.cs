4c1651771ab73037dc5db6ad1f4cd075c191d9ee
syntaxwhiz <zamaan.net.dev@gmail.com>
2022-06-13 20:43:10 +0500

    SqlConnectionWrapper : Added Methods

---
+++ Smartwyre.DeveloperTest/DB/SqlConnectionWrapper.cs
@@ -0,0 +1,27 @@
+﻿using Smartwyre.DeveloperTest;
+using System.Data.SqlClient;
+
+public class SqlConnectionWrapper : IDbConnectionWrapper
+{
+    private readonly SqlConnection sqlConnection;
+    public SqlConnectionWrapper()
+    {
+        string connectionString = Constants.CONNECTION_STRING;
+        sqlConnection = new SqlConnection(connectionString);
+    }
+
+    public void Open()
+    {
+        sqlConnection.Open();
+    }
+
+    public void Close()
+    {
+        sqlConnection.Close();
+    }
+
+    public SqlCommand CreateCommand()
+    {
+        return sqlConnection.CreateCommand();
+    }
+}
